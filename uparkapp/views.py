1a2c296e844da50dcbda755ba2d74e4ed3d8560a
John Jairo Montoya <jmontoya@celerik.com>
2024-02-13 23:47:54 -0500

    Archivos iniciales del proyecto

---
+++ uparkapp/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def login(request):
+    #return HttpResponse('<h1>Welcome to Upark login</h1>')
+    return render (request, 'login.html')
+
+def admin(request):
+    return render (request, 'admin.html')
+
+def user(request):
+    return render (request, 'user.html')
+
+def visitor(request):
+   return render (request, 'visitor.html')

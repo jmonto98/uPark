ba6c43345b7465e3adcc21256bd04e8772fa4dad
dposada13 <dposada13@gmail.com>
2024-03-05 19:20:05 -0500

    Chance main

--- uPark/settings.py
+++ uPark/settings.py
@@ -11,10 +11,13 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+MEDIA_ROOT= os.path.join (BASE_DIR,'media')
+MEDIA_URL='/media/'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
@@ -122,3 +125,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,"static"),
+    'uparkapp/static/',
+]

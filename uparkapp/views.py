b4e43b4316ae62f74c1ae2119c2e7ae181a70560
dposada13 <dposada13@gmail.com>
2024-04-02 17:58:09 -0500

    Changes views-card

--- uparkapp/views.py
+++ uparkapp/views.py
@@ -43,9 +43,7 @@ def adminuser(request):
     return render (request, 'adminuser.html')
 
 def card(request):
-    #userList= Person.objects.select_related ('idPerson').all()
-    cardList= Card.objects.select_related ('idPerson').all()
-    
+    cardList= Card.objects.select_related ('idPerson').all()    
     return render (request, 'card.html',{"Card": cardList})
 
 def visitor(request):
